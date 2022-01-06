d74804cfc52280cc1145b63a4028d975594762a9
pingu <jaicoco@naver.com>
2022-01-07 02:02:27 +0900

    [Combine] Subject Study

---
+++ Combine/Subject.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+// CurrentValueSubject
+//print("========CurrentValueSubject========")
+//let currentValueSubject = CurrentValueSubject<String, Never>("Pingu 첫번째 값")
+//let firstSubscription = currentValueSubject
+//    .sink(receiveCompletion: { print("1 번째 sink completion: \($0)") },
+//          receiveValue: { print("1 번째 sink value: \($0)") })
+//
+//currentValueSubject.send(completion: .finished)
+//
+//let secondSubscription = currentValueSubject
+//    .sink(receiveCompletion: { print("2 번째 sink completion: \($0)") },
+//          receiveValue: { print("2 번째 sink value: \($0)") })
+//
+//
+//// 현재 Subscriber들에게 모두 보냄
+//currentValueSubject.send("Pingu 두번째 값")
+////currentValueSubject.send(completion: .finished)
+//
+//print(currentValueSubject.value)
+
+// PassthroughSubject
+print("========PassthroughSubject========")
+
+let passthroughSubject = PassthroughSubject<String, Never>()
+let firstSubscription = passthroughSubject
+    .sink(receiveCompletion: { print("1번째 sink completion: \($0)") },
+          receiveValue: { print("1번째 sink value: \($0)")}
+    )
+
+passthroughSubject.send("PassthroughSubject 1번째 값")
+
+let secondSubscription = passthroughSubject
+    .sink(receiveCompletion: { print("2번째 sink completion: \($0)") },
+          receiveValue: { print("2번째 sink value: \($0)")}
+    )
+
+passthroughSubject.send("PassthroughSubject 2번째 값")
